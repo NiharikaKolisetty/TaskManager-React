49fcd9743babf08e44121cef153edcbbde669a3b
sriniharika <niharika@gmail.com>
2024-06-24 22:52:19 +0530

    2nd commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-
+import Logo from "../assets/Logo.png"
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header>
+        <div className="logo">
+            <img src={Logo} alt="Task Manager Logo"></img>
+            <span><b>Task Manager</b></span>
+        </div>
+        <div className="themeSelector">
+            <span className="light">Light</span>
+            <span className="dark">Dark</span>
+            <span className="gradient">Gradient</span>
+        </div>
+    </header>
   )
 }
 
