6f72c096862cdff2ff4b68b28d6ff1631cbb4835
sriniharika <niharika@gmail.com>
2024-06-27 03:25:55 +0530

    4th commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react"
 import Logo from "../assets/Logo.png"
 const Header = () => {
+  const[theme,setTheme] = useState(JSON.parse(localStorage.getItem("theme"))||"light");
+
+  useEffect(()=>{
+    localStorage.setItem("theme",JSON.stringify(theme))
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  },[theme])
   return (
     <header>
         <div className="logo">
@@ -7,9 +15,9 @@ const Header = () => {
             <span><b>Task Manager</b></span>
         </div>
         <div className="themeSelector">
-            <span className="light">Light</span>
-            <span className="dark">Dark</span>
-            <span className="gradient">Gradient</span>
+            <span onClick={()=> setTheme("light")} className={theme==="light" ? "light activeTheme" : "light"}>Light</span>
+            <span onClick={()=> setTheme("dark")} className={theme==="dark" ? "dark activeTheme" : "dark"}>Dark</span>
+            <span onClick={()=> setTheme("gradient")} className={theme==="gradient" ? "gradient activeTheme" : "gradient"}>Gradient</span>
         </div>
     </header>
   )
